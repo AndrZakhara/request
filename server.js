51005fc74c70aac14eb1c1443da6c98ac8084c76
Andrii Zakharchenko <art.pruttov@gmail.com>
2019-01-24 09:13:42 +0200

    initial commit

---
+++ server.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const fileUpload = require('express-fileupload');
+const app = express();
+const path = require('path');
+
+app.use('/form', express.static(path.join(__dirname, '/index.html')));
+app.use('/progress.js', express.static(path.join(__dirname, '/src/progress.js')));
+app.use('/HttpRequest.js', express.static(path.join(__dirname, '/src/HttpRequest.js')));
+app.use('/files', express.static(path.join(__dirname, '/src/uploads')));
+
+// default options
+app.use(fileUpload());
+
+app.post('/ping', function(req, res) {
+  res.send('pong');
+});
+
+app.post('/upload', function(req, res) {
+  let sampleFile = null;
+  let uploadPath = null;
+
+  if (Object.keys(req.files).length === 0) {
+    res.status(400).send('No files were uploaded.');
+    return;
+  }
+
+  console.log('req.files >>>', req.files); // eslint-disable-line
+
+  sampleFile = req.files.sampleFile; // eslint-disable-line
+
+  uploadPath = path.join(__dirname, '/uploads/', sampleFile.name);
+
+  sampleFile.mv(uploadPath, function(err) {
+    if (err) {
+      return res.status(500).send(err);
+    }
+
+    res.send(path.join('File uploaded to ', uploadPath));
+  });
+});
+
+app.listen(8000, function() {
+  console.log('Express server listening on port 8000'); // eslint-disable-line
+});
