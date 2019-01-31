f6fa23a05eb6e879c8d8cacc6a3d85aca76250e8
Andrii Zakharchenko <art.pruttov@gmail.com>
2019-01-31 15:51:21 +0200

    feature/add_img, add img render

--- src/Components/DownLoadForm.js
+++ src/Components/DownLoadForm.js
@@ -2,26 +2,27 @@ import { createElement } from '../helpers/utils';
 import HttpRequest from '../libs/HttpRequest';
 import { onDownloadProgress } from './ProgressBar';
 
-function downloadFile(data, fileName) {
-  const elementLink = document.createElement('a');
-  const fileURL = URL.createObjectURL(data);
-  elementLink.href = fileURL;
-  elementLink.download = fileName;
-  elementLink.click();
+function appendImage(url) {
+  const element = document.querySelector('.wrapper-download-img');
+  const img = document.createElement('img');
+  img.src = url;
+  img.className = 'download-img';
+  element.appendChild(img);
 }
 
-// function appendImage(url) {
-//   const node = document.querySelector('.download-img');
-//
-//   if (node) {
-//     node.src = url;
-//   } else {
-//     const img = document.createElement('img');
-//     img.src = url;
-//     img.className = 'download-img';
-//     document.body.appendChild(img);
-//   }
-// }
+function handleData(data, fileName) {
+  const fileURL = URL.createObjectURL(data);
+  const { type } = data;
+
+  if (type === 'image/png' || type === 'image/jpeg' || type === 'image/gif') {
+    appendImage(fileURL);
+  } else {
+    const elementLink = document.createElement('a');
+    elementLink.href = fileURL;
+    elementLink.download = fileName;
+    elementLink.click();
+  }
+}
 
 export default function createElementDownloadForm() {
   let inputDownloadValue = null;
@@ -64,8 +65,15 @@ export default function createElementDownloadForm() {
     const req = new HttpRequest({ baseUrl: 'http://localhost:8000' });
     req.get(`/files/${inputDownloadValue}`, { responseType: 'blob', onDownloadProgress })
       .then(data => {
-        console.log(data.response); // eslint-disable-line
-        downloadFile(data.response, inputDownloadValue);
+        const { type } = data.response;
+        console.log(data.response.type); // eslint-disable-line
+
+        handleData(data.response, inputDownloadValue);
+        // if (type === 'image/png' || type === 'image/jpeg' || type === 'image/gif') {
+        //   appendImage(data.response)
+        // } else {
+        //   downloadFile(data.response, inputDownloadValue);
+        // }
       });
   };
 
--- src/index.js
+++ src/index.js
@@ -8,12 +8,14 @@ import createMainForm from './Components/FormWrapper';
 function createApp() {
   const mainForm = createMainForm();
   const filesList = createListOfFiles();
+  const mainImgDiv = createElement('div', { className: 'wrapper-download-img' });
 
   const mainAppWrapper = createElement(
     'div',
     { className: 'main-wrapper' },
     filesList,
     mainForm,
+    mainImgDiv
   );
 
   document.querySelector('.app').appendChild(mainAppWrapper);
