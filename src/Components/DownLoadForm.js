eb704133bf11dc1dbde85d4654185db22856448a
Andrii Zakharchenko <art.pruttov@gmail.com>
2019-01-31 11:12:13 +0200

    feature/download_form, add event handlers

--- src/Components/DownLoadForm.js
+++ src/Components/DownLoadForm.js
@@ -1,6 +1,30 @@
 import { createElement } from '../helpers/utils';
+import HttpRequest from '../libs/HttpRequest';
+import { onDownloadProgress } from './ProgressBar';
+
+function downloadFile(data, fileName) {
+  const elementLink = document.createElement('a');
+  const fileURL = URL.createObjectURL(data);
+  elementLink.href = fileURL;
+  elementLink.download = fileName;
+  elementLink.click();
+}
+
+// function appendImage(url) {
+//   const node = document.querySelector('.download-img');
+//
+//   if (node) {
+//     node.src = url;
+//   } else {
+//     const img = document.createElement('img');
+//     img.src = url;
+//     img.className = 'download-img';
+//     document.body.appendChild(img);
+//   }
+// }
 
 export default function createElementDownloadForm() {
+  let inputDownloadValue = null;
   const inputTypeTextDownload = createElement(
     'input',
     {
@@ -18,12 +42,32 @@ export default function createElementDownloadForm() {
     }
   );
 
+  inputTypeTextDownload.onchange = e => {
+    inputDownloadValue = e.target.value;
+    // document.querySelector('.progress').style.display = 'block';
+    // document.querySelector('.progress-notificatione-download').style.display = 'block';
+  };
+
   const downloadForm = createElement(
     'form',
     { id: 'downloadForm', encType: 'multipart/form-data' },
     inputTypeTextDownload,
     inputTypeSubmitDownload
   );
+
+  downloadForm.onsubmit = function(e) {
+    e.preventDefault();
+    const myHeaders = new Headers();
+
+    myHeaders.append('Content-Type', 'multipart/form-data');
+
+    const req = new HttpRequest({ baseUrl: 'http://localhost:8000' });
+    req.get(`/files/${inputDownloadValue}`, { responseType: 'blob', onDownloadProgress })
+      .then(data => {
+        console.log(data.response); // eslint-disable-line
+        downloadFile(data.response, inputDownloadValue);
+      });
+  };
 
   return downloadForm;
 }
--- src/Components/UploadForm.js
+++ src/Components/UploadForm.js
@@ -5,7 +5,7 @@ import { onUploadProgress } from './ProgressBar';
 export default function createElementUploadForm() {
   const hendlerOnChangeSelectUploadFile = e => {
     e.preventDefault();
-    console.log(e.target.files.item(0).name); // eslint-disable-line
+
     document.querySelector('.upload-file-name').innerHTML = e.target.files.item(0).name;
     document.querySelector('.progress').style.display = 'block';
     document.querySelector('.progress-notificatione').style.display = 'block';
@@ -17,12 +17,11 @@ export default function createElementUploadForm() {
   };
 
   const inputTypeFileUpload = createElement('input', { className: 'file-upload', type: 'file', name: 'sampleFile' });
-
   inputTypeFileUpload.onchange = e => hendlerOnChangeSelectUploadFile(e);
 
   const inputTypeSubmitUpload = createElement('input', { className: 'form-button', type: 'submit', value: 'Upload' });
-  const buttonSelectFile = createElement('button', { className: 'form-button select-button' }, 'Select file');
 
+  const buttonSelectFile = createElement('button', { className: 'form-button select-button' }, 'Select file');
   buttonSelectFile.onclick = e => hendlerClickSelectUploadFile(e);
 
   const elementTextFileName = createElement('span', { className: 'upload-file-name' }, 'No file selected...');
@@ -39,14 +38,17 @@ export default function createElementUploadForm() {
     e.preventDefault();
     const form = new FormData();
     const myHeaders = new Headers();
+    const [file] = e.target.sampleFile.files;
 
     myHeaders.append('Content-Type', 'multipart/form-data');
-    form.append('sampleFile', e.target.sampleFile.files[0]);
+    form.append('sampleFile', file);
 
     const req = new HttpRequest({ baseUrl: 'http://localhost:8000' });
 
     req.post('/upload', { responseType: 'json', data: form, onUploadProgress })
-      .then(data => console.log(data.status)); // eslint-disable-line
+      .then(data => {
+        console.log(data.status) // eslint-disable-line
+      });
   };
 
   return uploadForm;
